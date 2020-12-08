e6b1f1a9f24cb14ea4f78f8eec9f12ffea6eb991
jandedeuraerder-howest <jan.de.deurwaerder@howest.be>
2020-12-08 14:08:46 +0100

    Add project files.

---
+++ Scala.Les4.Wpf/MainWindow.xaml.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Scala.Les4.Wpf
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+        }
+
+        private void btnHeeftBetaald_Click(object sender, RoutedEventArgs e)
+        {
+        }
+
+        private void btnHeeftNietBetaald_Click(object sender, RoutedEventArgs e)
+        {
+        }
+
+        private void btnKleinKind_Click(object sender, RoutedEventArgs e)
+        {
+        }
+
+        private void btnGrootKind_Click(object sender, RoutedEventArgs e)
+        {
+        }
+
+
+
+        private void btnGeta1GroterDanGetal2_Click(object sender, RoutedEventArgs e)
+        {
+        }
+
+        private void btnGeta1KleinerDanGetal2_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnGeta1GelijkAanGetal2_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnAllesInEenKeer_Click(object sender, RoutedEventArgs e)
+        {
+
+
+        }
+        private void btnBerekenTotaalPrijs_Click(object sender, RoutedEventArgs e)
+        {
+            // indien totaalprijs > 500 en aantal > 5 => korting geven
+
+
+        }
+        private void btnBerekenTotaalPrijs2_Click(object sender, RoutedEventArgs e)
+        {
+            // indien totaalprijs > 500 EN aantal > 5 => korting geven
+            // OF klant komt uit België => korting geven
+
+        }
+    }
+}
